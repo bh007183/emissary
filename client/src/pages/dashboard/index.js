30edc7ae7a7722e176ca4e0d4e585f18217d168f
Ben Hopkins <bjhops17@gmail.com>
2021-07-27 09:15:34 -0700

    added base dashboard

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,22 +1,48 @@
-import React, {useEffect, useState} from 'react'
-import {useDispatch, useSelector} from "react-redux"
-import {clearRoute} from "../../store/userActions"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearRoute } from "../../store/userActions";
+import "./style.css";
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
 
 export default function UserDash() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-    
-    console.log("render?")
-   
-    useEffect(() => {
-        dispatch(clearRoute())
-       console.log("test")
+  useEffect(() => {
+    dispatch(clearRoute());
+    console.log("test");
+  }, []);
 
-    }, [])
-    
-    return (
-        <div>
-            You are logged in
-        </div>
-    )
+  const handleKeyDown = (e) =>{
+    e.target.style.height = 'inherit';
+    e.target.style.height = `${Math.min(e.target.scrollHeight, 80)}px`;
+  }
+
+  return (
+    <div id="dashBoard">
+      <div id="roomColumn">hey</div>
+      <div id="messageController">
+        <div id="formContainer">
+          <form id="messageBar">
+             
+            <textarea
+              id="input"
+              placeholder="type here..."
+              rows="2"
+              onKeyUp={handleKeyDown}
+            >  
+            </textarea>
+            <div id="formBar">
+                <IconButton>
+                    <SendIcon />
+                </IconButton>
+
+            </div>
+          </form>
+          </div>
+     
+        ho
+      </div>
+    </div>
+  );
 }
