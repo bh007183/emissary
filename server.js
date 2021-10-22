20fea258f6cdb7477de80257afbb04c242dd9cad
Ben Hopkins <bjhops17@gmail.com>
2021-10-22 09:10:13 -0700

    forgot to add corsOptions

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var corsOptions = {
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 io.on("connection", (socket) => {
